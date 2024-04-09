4e11c66bfb45ecd4bf0afc2c5d236553a3e04116
isimmons <isimmons33@gmail.com>
2024-04-09 17:16:56 -0400

    install and configure basic md editor (tiptap)

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -7,6 +7,7 @@ import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import InputError from "@/Components/InputError.vue";
 import TextArea from "@/Components/TextArea.vue";
+import MarkdownEditor from "@/Components/MarkdownEditor.vue";
 
 const postForm = useForm({
     'title': '',
@@ -34,7 +35,8 @@ const createPost = () => postForm.post(route('posts.store'));
                         <InputLabel for="body"
                                     class="sr-only">Body
                         </InputLabel>
-                        <TextArea id="body" v-model="postForm.body" rows="25"  />
+                        <MarkdownEditor v-model="postForm.body" />
+                        <TextArea id="body" v-model="postForm.body" rows="25" class="mt-2"  />
                         <InputError :message="postForm.errors.body" class="mt-1"/>
                     </div>
                     <div>

d9b9c425dde52bf78d43a7c551b2dc9c5af56578
isimmons <isimmons33@gmail.com>
2024-04-10 00:43:06 -0400

    make trait more flexible

--- app/Models/Concerns/ConvertsMarkdownToHtml.php
+++ app/Models/Concerns/ConvertsMarkdownToHtml.php
@@ -13,10 +13,24 @@ trait ConvertsMarkdownToHtml
 {
     protected static function bootConvertsMarkdownToHtml(): void
     {
-        static::saving(fn ( self $model) => $model->fill(['html' => str($model->body)->markdown([
-            'html_input' => 'strip',
-            'allow_unsafe_links' => false,
-            'max_nesting_level' => 5,
-        ])]));
+        static::saving(function ( self $model) {
+
+            $markdownData = collect(self::getMarkdownToHtmlMap())
+                ->flip()
+                ->map(fn ($bodyColumn) => str($model->$bodyColumn)->markdown([
+                    'html_input' => 'strip',
+                    'allow_unsafe_links' => false,
+                    'max_nesting_level' => 5,
+                ]));
+
+            return $model->fill($markdownData->all());
+        });
+    }
+
+    protected static function getMarkdownToHtmlMap(): array
+    {
+        return [
+            'body' => 'html'
+        ];
     }
 }

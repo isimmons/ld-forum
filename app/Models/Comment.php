21b83bb58e7b054bec367bf998657a7da522da26
isimmons <isimmons33@gmail.com>
2024-04-10 00:46:02 -0400

    convert comment markdown to html (backend)

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Concerns\ConvertsMarkdownToHtml;
 
 class Comment extends Model
 {
     use HasFactory;
+    use ConvertsMarkdownToHtml;
 
     protected $guarded = [];
     public function user(): BelongsTo
---
+++ tests/Feature/Models/CommentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\Comment;
+
+it('should generate html from new and updated comments', function () {
+    $comment = Comment::factory()->make([ 'body' => '## Hello world' ]);
+
+    $comment->save();
+
+    expect($comment->html)->toEqual(str($comment->body)->markdown());
+});
